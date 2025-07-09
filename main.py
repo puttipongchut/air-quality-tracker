4d0a1bb0095a9e59823f29fae276f7f7aae662c0
puttipongchut <puttipongyrc@gmail.com>
2025-07-10 02:48:04 +0700

    pretty print json

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ COORDINATES = "13.75739,100.50662" # Center of Bangkok
 RADIUS = "12000" # Radius size around the coordinates
 TARGET_CITY = "Bangkok"
 TARGET_COUNTRY = "TH"
-TARGET_PARAMETER = "pm25"
-DAYS_TO_FETCH = 7
+TARGET_PARAMETER = "pm25" # Example 'pm25', 'pm10', 'o3', 'no2', 'so2', 'co'
+LIMITS = 3
 
 def get_air_quality_data():
     if not OPENAQ_API_KEY:
@@ -26,18 +26,19 @@ def get_air_quality_data():
 
     # Fetch Data
     try:
-        url = f"{BASE_URL}/locations?coordinates={COORDINATES}&radius={RADIUS}"
+        url = f"{BASE_URL}/locations?coordinates={COORDINATES}&radius={RADIUS}&parameter={TARGET_PARAMETER}&limit={LIMITS}"
         headers = { "X-API-KEY": OPENAQ_API_KEY }
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         response_data = response.json()
 
         # Filter the JSON
+        import json
         raw_measurements = response_data.get('results', [])
-        print(raw_measurements)
+        print(json.dumps(raw_measurements, indent=2))
         
     except requests.exceptions.RequestException as e:
-            print(f"Error fetching measurements: {e}")
+            print(f"Error fetching locations: {e}")
             return
 
 if __name__ == "__main__":
